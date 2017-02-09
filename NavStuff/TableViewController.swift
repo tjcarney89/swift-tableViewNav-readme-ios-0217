85a17c0b4183de51d011954d8a88931ff696582c
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-02-09 17:46:10 -0500

    Completed Table View and Ran App

--- NavStuff/TableViewController.swift
+++ NavStuff/TableViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    let dirtyDancingSoundtrack: [String] = [
+        "(I've Had) The Time of My Life",
+        "Be My Baby",
+        "She's Like the Wind",
+        "Hungry Eyes",
+        "STAY",
+        "Yes",
+        "You Don't Own Me",
+        "Hey Baby",
+        "Overload",
+        "Love Is Strange",
+        "Where Are You Tonight",
+        "In the Still of the Night"
+    ]
+    
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return dirtyDancingSoundtrack.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DirtyCell", for: indexPath)
+        
+        let dirtyDancingSong = dirtyDancingSoundtrack[indexPath.row]
+        
+        cell.textLabel?.text = dirtyDancingSong
+        
+        return cell
     }
 }
